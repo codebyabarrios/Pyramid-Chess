1c0e248cf153d5c8bad0130df45b80969c0cff0a
Agustin Barrios <agustinbarrios1000@gmail.com>
2026-06-25 00:28:44 -0300

    Añadiendo los peones blancos y negros al tablero con sus respectivas posiciones, tambien he creado una carpeta para ir guardando los diferentes pixels art de tanto las piezas como de las casillas del juego

--- scenes/board.gd
+++ scenes/board.gd
@@ -5,6 +5,10 @@ const TILE_SIZE = 64
 
 @onready var white_tile_texture=preload("res://Assets juego/tile_white.png")
 @onready var black_tile_texture=preload("res://Assets juego/tile_black.png")
+@onready var pawn_scene=preload("res://scenes/pawn/pawn.tscn")
+@onready var white_pawn_tex="res://scenes/pawn/white_pawn.png"
+@onready var black_pawn_tex="res://scenes/pawn/black_pawn.png"
+
 var board = []
 
 func _ready():
@@ -15,17 +19,28 @@ func create_board():
 	board.clear()
 	for y in range(BOARD_SIZE):
 		board.append([])
+		
 		for x in range(BOARD_SIZE):
-			board[y].append(null)
+			
+			var current_piece = null
+			
+			if y == 1:
+				current_piece = pawn_scene.instantiate()
+				current_piece.set_side(false, black_pawn_tex)
+				current_piece.grid_position = Vector2i(x, y)
+			
+			elif y == 6:
+				current_piece = pawn_scene.instantiate()
+				current_piece.set_side(true, white_pawn_tex)
+				current_piece.grid_position = Vector2i(x, y)
+				
+			board[y].append(current_piece)
 			
 			var new_tile = Sprite2D.new()
-			if(x+ y)%2==0:
+			if(x + y) % 2 == 0:
 				new_tile.texture = white_tile_texture
 			else:
-				new_tile.texture=black_tile_texture
-			
-			new_tile.scale = Vector2(1, 1) 
-			
+				new_tile.texture = black_tile_texture
 			
 			new_tile.position = Vector2(
 				(x * TILE_SIZE) + (TILE_SIZE / 2),
@@ -33,6 +48,11 @@ func create_board():
 			)
 			
 			add_child(new_tile)
+			
+			if board[y][x] != null:
+				board[y][x].position = new_tile.position
+				add_child(board[y][x])
+
 
 func print_board():
 	for row in board:
