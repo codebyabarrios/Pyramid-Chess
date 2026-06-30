9c1d2081049d3b556152cba913ce3228061c0560
Agustin Barrios <agustinbarrios1000@gmail.com>
2026-06-30 15:37:40 -0300

    Modificando la disposicion de los peones en el tablero para que se ubiquen en la penúltima y antepenúltima fila de manera alternada

--- scenes/board.gd
+++ scenes/board.gd
@@ -3,8 +3,8 @@ extends Node2D
 const BOARD_SIZE = 8
 const TILE_SIZE = 64
 
-@onready var white_tile_texture=preload("res://Assets juego/tile_white.png")
-@onready var black_tile_texture=preload("res://Assets juego/tile_black.png")
+@onready var white_tile_texture=preload("res://Game assets/tile_white.png")
+@onready var black_tile_texture=preload("res://Game assets/tile_black.png")
 @onready var pawn_scene=preload("res://scenes/pawn/pawn.tscn")
 @onready var white_pawn_tex="res://scenes/pawn/white_pawn.png"
 @onready var black_pawn_tex="res://scenes/pawn/black_pawn.png"
@@ -16,42 +16,42 @@ func _ready():
 	print_board()
 
 func create_board():
-	board.clear()
-	for y in range(BOARD_SIZE):
-		board.append([])
+		board.clear()
+		for y in range(BOARD_SIZE):
+			board.append([])
 		
-		for x in range(BOARD_SIZE):
+			for x in range(BOARD_SIZE):
 			
-			var current_piece = null
+				var current_piece = null
 			
-			if y == 1:
-				current_piece = pawn_scene.instantiate()
-				current_piece.set_side(false, black_pawn_tex)
-				current_piece.grid_position = Vector2i(x, y)
+				if y == 5 or y == 6:
+						current_piece = pawn_scene.instantiate()
+						current_piece.grid_position = Vector2i(x, y)
+						if (x + y) % 2 == 0:
+								current_piece.set_side(true, white_pawn_tex)
+						else:
+								current_piece.set_side(false, black_pawn_tex)
+				else:
+					current_piece = null
+					
+				board[y].append(current_piece)
 			
-			elif y == 6:
-				current_piece = pawn_scene.instantiate()
-				current_piece.set_side(true, white_pawn_tex)
-				current_piece.grid_position = Vector2i(x, y)
-				
-			board[y].append(current_piece)
+				var new_tile = Sprite2D.new()
+				if(x + y) % 2 == 0:
+					new_tile.texture = white_tile_texture
+				else:
+					new_tile.texture = black_tile_texture
 			
-			var new_tile = Sprite2D.new()
-			if(x + y) % 2 == 0:
-				new_tile.texture = white_tile_texture
-			else:
-				new_tile.texture = black_tile_texture
+				new_tile.position = Vector2(
+					(x * TILE_SIZE) + (TILE_SIZE / 2),
+					(y * TILE_SIZE) + (TILE_SIZE / 2)
+				)
 			
-			new_tile.position = Vector2(
-				(x * TILE_SIZE) + (TILE_SIZE / 2),
-				(y * TILE_SIZE) + (TILE_SIZE / 2)
-			)
+				add_child(new_tile)
 			
-			add_child(new_tile)
-			
-			if board[y][x] != null:
-				board[y][x].position = new_tile.position
-				add_child(board[y][x])
+				if board[y][x] != null:
+					board[y][x].position = new_tile.position
+					add_child(board[y][x])
 
 
 func print_board():
