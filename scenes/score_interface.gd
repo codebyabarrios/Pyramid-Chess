399bf4ca59ab982251242cded8c506ea49da0b23
Agustin Barrios <agustinbarrios1000@gmail.com>
2026-07-10 21:57:57 -0300

    Modificando el tipo de letra que se muestra en pantalla de los jugadores por Press Start 2P

--- scenes/score_interface.gd
+++ scenes/score_interface.gd
@@ -4,5 +4,5 @@ extends Control
 @onready var black_label: Label = $PanelContainer/VBoxContainer/BlackLabel
 
 func _process(_delta: float) -> void:
-	white_label.text  = "White Rider" + str(round(Gamemanager.white_points)) + " pts"
-	black_label.text = "Black Rider" + str(round(Gamemanager.black_points)) + " pts"
+	white_label.text  = "White Rider: " + str(round(Gamemanager.white_points)) + " pts"
+	black_label.text = "Black Rider: " + str(round(Gamemanager.black_points)) + " pts"
